6a3d6be05a166806e9e82301f47c71f736a116ca
Koudai <music.orist.7@gmail.com>
2020-09-26 05:34:22 +0900

    modify header home link

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'web_urls#home'
 
   get '/home', to: 'web_urls#home'
-  get '/web_urls/web_urls#home', to: 'web_urls#home'
+  #get '/web_urls/web_urls#home', to: 'web_urls#home'
   #get 'web_urls/show'
   get '/input_urls', to: 'web_urls#input_url'
   get '/show', to: 'web_urls#show'
