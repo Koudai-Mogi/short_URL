b4bca09ce1f4e50eba2a43829089916ccefc2a04
Koudai <music.orist.7@gmail.com>
2020-09-25 23:40:31 +0900

    add input view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    # ページごとの完全なタイトルを返します。
+ def full_title(page_title = '') # メソッド定義とオプション引数
+   base_title = "URL短縮化サービス" # 変数への代入
+   if page_title.empty?  # 論理値テスト
+     base_title
+   else
+     page_title + " | " + base_title  # 文字列の結合
+   end
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'web_url#home'
 
   get 'web_url/show'
-
+  get 'input_url', to: 'web_url#input_url'
+  get 'show', to: 'web_url#show' 
   # root 'application#home'
 end
