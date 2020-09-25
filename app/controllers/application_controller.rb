3c654e79ad90e017fc1739f162bc8eb143cc4b09
Koudai <music.orist.7@gmail.com>
2020-09-25 21:35:28 +0900

    add home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def home
-      render "hello,world,home画面だよ"
+      render html:"hello,world,home画面だよ"
   end
 
 end
